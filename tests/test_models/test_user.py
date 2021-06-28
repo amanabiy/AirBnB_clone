06b6f303e44da69d1224ca0456278f3deb48fb57
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-06-28 20:30:19 +0000

    test file for class definitions

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,20 +1,60 @@
 #!/usr/bin/python3
-"""Unittest Module for Base class
-"""
 import unittest
-from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
+from models.user import User
+"""
+Unittest Module for User class
+"""
 
 
-class TestBase(unittest.TestCase):
-    """Unittest for Base class"""
+class TestUser(unittest.TestCase):
+    ''' Unittest for User class '''
 
-    def test_nb_onject_Instantiation(self):
-        """instantiates class"""
-        Base._Base__nb_objects = 0
-        pass
+    def test_object_Instantiation(self):
+        ''' instantiates class '''
+        self.user = User()
+        
+    def testattr(self):
+        ''' test Class: User attributes '''
+        self.user = User()
+        self.assertTrue(hasattr(self.user, "created_at"))
+        self.assertTrue(hasattr(self.user, "updated_at"))
+        self.assertFalse(hasattr(self.user, "random_attr"))
+        self.assertFalse(hasattr(self.user, "name"))
+        self.assertTrue(hasattr(self.user, "id"))
+        self.user.name = "Alice"
+        self.user.age = "44"
+        self.assertTrue(hasattr(self.user, "name"))
+        self.assertTrue(hasattr(self.user, "age"))
+        delattr(self.user, "name")
+        self.assertFalse(hasattr(self.user, "name"))
+        delattr(self.user, "age")
+        self.assertFalse(hasattr(self.user, "age"))
+        self.assertEqual(self.user.email, "")
+        self.assertEqual(self.user.password, "")
+        self.assertEqual(self.user.first_name, "")
+        self.assertEqual(self.user.last_name, "")
+        self.assertEqual(self.user.__class__.__name__, "User")
+        self.user.first_name = "Alice"
+        self.user.last_name = "Bob"
+        self.user.email = "Alice@gmail.com"
+        self.user.password = "Al23ba4f$58"
+        self.assertEqual(self.user.email, "Alice@gmail.com")
+        self.assertEqual(self.user.password, "Al23ba4f$58")
+        self.assertEqual(self.user.first_name, "Alice")
+        self.assertEqual(self.user.last_name, "Bob")
 
-    def test_nb_objects_private(self):
-        """test nb_objects is private"""
-        self.assertTrue(hasattr(Base, "_Base__nb_objects"))
+    def testsave(self):
+        ''' testing method: save '''
+        self.user = User()
+        self.user.save()
+        self.assertTrue(hasattr(self.user, "updated_at"))
+
+    def teststr(self):
+        ''' testing __str__ return format of User '''
+        self.user = User()
+        s = "[{}] ({}) {}".format(self.user.__class__.__name__,
+                                    str(self.user.id), self.user.__dict__)
+        self.assertEqual(print(s), print(self.user))
+
+if __name__ == '__main__':
+    unittest.main()
