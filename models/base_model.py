dad2ad0b279c59e825b8562378224edccb59ea92
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-06-29 18:55:11 +0000

    definition for AirBnB utility classes

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,19 @@ methods for other classes
 from uuid import uuid4
 from datetime import datetime
 from models import storage
-import json, sys, os.path
+import json
+import sys
+import os.path
 
 
 class BaseModel():
-    """ a base class for other classes """
+    ''' a base class for other classes '''
     dtf = '%Y-%m-%dT%H:%M:%S.%f'
 
     def __init__(self, *args, **kwargs):
-        """ initializes the values """
+        '''
+        initializes the values
+        '''
         if kwargs != {}:
             for key, val in kwargs.items():
                 if "created_at" == key:
@@ -35,39 +39,41 @@ class BaseModel():
             storage.new(self)
 
     def __str__(self):
-        """ print in "[<class name>] (<self.id>) <self.__dict__>" format"""
+        '''
+        print in "[<class name>] (<self.id>) <self.__dict__>" format
+        '''
         return ("[{}] (<{}>) <{}>".format(
             self.__class__.__name__,
             self.id,
             self.__class__.__dict__))
 
     def save(self):
-        """
+        '''
         updates the public instance attribute updated_at
         with the current datetime
-        """
+        '''
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """
+        '''
         returns a dictionary containing all keysvalues
         of __dict__ of the instance
-        """        
+        '''
         my_dict = dict(self.__dict__)
         my_dict["__class__"] = type(self).__name__
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         return my_dict
-        
+
     def to_json(self):
-        """
+        '''
         returns a json containing all keysvalues
         of __dict__ of the instance
-        """        
+        '''
         my_json = self.__dict__.copy()
         my_json.update({'created_at': self.created_at.strftime(self.dtf)})
         my_json.update({'__class__': str(self.__class__.__name__)})
         if hasattr(self, 'updated_at'):
             my_json.update({'updated_at': self.updated_at.strftime(self.dtf)})
-        return my_json 
+        return my_json
